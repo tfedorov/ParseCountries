4b00feb719daf97a520d7044d64dca28b25547d5
Dependency <aelrismira@gmail.com>
2018-10-03 23:05:47 +0300

    -sum of population

--- src/main/scala/com/epam/sparkV2/HelloSparkRDD2.scala
+++ src/main/scala/com/epam/sparkV2/HelloSparkRDD2.scala
@@ -1,18 +1,22 @@
 package com.epam.sparkV2
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
 
-object HelloSparkRDD {
+
+
+object HelloSparkRDD2 {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("simpleReading").setMaster("local[*]")
     val sc: SparkContext = new SparkContext(conf)
 
-    var sum: Long = 0
+
     val distFile: RDD[String] = sc.textFile("src\\main\\scala\\resourses\\countries_of_the_world.csv")
-    println("                      ")
-    distFile.map(obj => println(obj(0).toString))
-    println("                      ")
+
+    val value: RDD[String] = distFile.map(_.split(",")(2))
+      .filter(str => !str.contains("Population"))
+   val rdd = value.map(_.toLong)
+    println(rdd.take(rdd.count().toInt).sum)
 
   }
 
